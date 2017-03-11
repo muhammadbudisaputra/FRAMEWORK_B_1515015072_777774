20da2b972596c05643449613fda1993b9a0780c6
muhammadbudisaputra <budisaputra34@gmail.com>
2017-03-11 17:25:17 +0700

    Tugas Framework Selesai

---
+++ laravel5/app/Http/routes.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('hello-world', function () {
+    return 'Hello World';
+});
+Route::get('pengguna/{pengguna}', function ($pengguna) {
+    return "Hello World Dari Pengguna $pengguna";
+});
+Route::get('framework/{mhs?}', function ($mhs="Muhammad Budi Saputra") {
+    return "Hello $mhs Dari Pengguna";
+});
