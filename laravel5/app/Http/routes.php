f45823511c9c0fe69010fc52ead8bbcea8d431c9
muhammadbudisaputra <budisaputra34@gmail.com>
2017-03-24 09:52:10 +0700

    Tugas 4 Selesai

--- laravel5/app/Http/routes.php
+++ laravel5/app/Http/routes.php
@@ -11,27 +11,42 @@
 |
 */
 
-Route::get('hello-world', function () {
-    return 'Hello World';
+Route::get('/', function () {
+    return view('welcome');
 });
-Route::get('pengguna/{pengguna}', function ($pengguna) {
-    return "Hello World Dari Pengguna $pengguna";
-});
-Route::get('framework/{mhs?}', function ($mhs="Muhammad Budi Saputra") {
-    return "Hello $mhs Dari Pengguna";
-});
-Route::get('Jarvis','JarvisController@awal');
-Route::get('Jarvis/tambah','JarvisController@tambah');
-Route::get('dosen','dosenController@awal');
-Route::get('dosen/tambah','dosenController@tambah');
-Route::get('mahasiswa','mahasiswaController@awal');
-Route::get('mahasiswa/tambah','mahasiswaController@tambah');
-Route::get('matakuliah','matakuliahController@awal');
-Route::get('matakuliah/tambah','matakuliahController@tambah');
-Route::get('dosenmatakuliah','dosen_matakuliahController@awal');
-Route::get('dosenmatakuliah/tambah','dosen_matakuliahController@tambah');
-Route::get('jadwalmatakuliah','jadwal_matakuliahController@awal');
-Route::get('jadwalmatakuliah/tambah','jadwal_matakuliahController@tambah');
-Route::get('ruangan','ruanganController@awal');
-Route::get('ruangan/tambah','ruanganController@tambah');
+
+Route::get('pengguna', 'PenggunaController@awal');
+Route::get('pengguna/tambah', 'PenggunaController@tambah');
+Route::get('pengguna/{pengguna}','PenggunaController@lihat');
+Route::post('pengguna/simpan','PenggunaController@simpan');
+Route::get('pengguna/edit/{pengguna}','PenggunaController@edit');
+Route::post('pengguna/edit/{pengguna}','PenggunaController@update');
+Route::get('pengguna/hapus/{pengguna}','PenggunaController@hapus');
+
+Route::get('Dosen', 'DosenController@awal');
+Route::get('Dosen/tambah', 'DosenController@tambah');
+Route::get('Mahasiswa', 'MahasiswaController@awal');
+Route::get('Mahasiswa/tambah', 'MahasiswaController@tambah');
+
+Route::get('ruangan', 'ruangancontroller@awal');
+Route::get('ruangan/tambah', 'ruangancontroller@tambah');
+Route::get('ruangan/{ruangan}','ruangancontroller@lihat');
+Route::post('ruangan/simpan','ruangancontroller@simpan');
+Route::get('ruangan/edit/{ruangan}','ruangancontroller@edit');
+Route::post('ruangan/edit/{ruangan}','ruangancontroller@update');
+Route::get('ruangan/hapus/{ruangan}','ruangancontroller@hapus');
+
+Route::get('matakuliah', 'matakuliahcontroller@awal');
+Route::get('matakuliah/tambah', 'matakuliahcontroller@tambah');
+Route::get('matakuliah/{matakuliah}','matakuliahcontroller@lihat');
+Route::post('matakuliah/simpan','matakuliahcontroller@simpan');
+Route::get('matakuliah/edit/{matakuliah}','matakuliahcontroller@edit');
+Route::post('matakuliah/edit/{matakuliah}','matakuliahcontroller@update');
+Route::get('matakuliah/hapus/{matakuliah}','matakuliahcontroller@hapus');
+
+Route::get('Dosen_matakuliah', 'Dosen_matakuliahController@awal');
+Route::get('Dosen_matakuliah/tambah', 'Dosen_matakuliahController@tambah');
+
+Route::get('Jadwal_matakuliah', 'Jadwal_matakuliahController@awal');
+Route::get('Jadwal_matakuliah/tambah', 'Jadwal_matakuliahController@tambah');
 
